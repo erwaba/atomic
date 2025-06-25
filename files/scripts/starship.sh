31721abe56bc8a1f2a9cdf67a329f1e1eb90e3ef
erwaba <183932306+erwaba@users.noreply.github.com>
2025-06-25 16:04:05 -0700

    Install starship and make default using /etc/bashrc.

---
+++ files/scripts/starship.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# This script content taken from https://github.com/ublue-os/bluefin/blob/main/build_files/base/05-override-install.sh June 2025
+
+# Starship Shell Prompt
+curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >>/etc/bashrc
+
